b6c2b7d5b7b47c59b4f6434e82a5e8c907cd2305
yibeomseok <vv99911@gmail.com>
2025-06-20 18:11:54 +0900

    feat: BufferStep 추가

---
+++ fipe-core/src/commonMain/kotlin/fipe/step/BufferStep.kt
@@ -0,0 +1,25 @@
+package fipe.step
+
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
+
+
+interface BufferStep<T> : Step<T, T> {
+    val capacity: Int
+    val onBufferOverflow: BufferOverflow
+}
+
+fun <T> BufferStep(
+    capacity: Int,
+    onBufferOverflow: BufferOverflow = BufferOverflow.SUSPEND,
+    name: String = "BufferStep",
+): BufferStep<T> = BufferStepImpl(capacity, onBufferOverflow, name)
+
+private class BufferStepImpl<T>(
+    override val capacity: Int,
+    override val onBufferOverflow: BufferOverflow = BufferOverflow.SUSPEND,
+    override val name: String = "BufferStep",
+) : BufferStep<T> {
+    override fun process(flow: Flow<T>): Flow<T> = flow.buffer(capacity, onBufferOverflow)
+}
